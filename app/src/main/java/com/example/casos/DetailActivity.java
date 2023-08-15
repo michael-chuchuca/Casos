8688786d1af8fe942b965b8f7785ed8f1af59e31
michael-chuchuca <michael.chuchucab@ug.edu.ec>
2023-08-14 19:32:33 -0500

    Upload Casos

---
+++ app/src/main/java/com/example/casos/DetailActivity.java
@@ -0,0 +1,21 @@
+package com.example.casos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.casos.databinding.ActivityDetailBinding;
+public class DetailActivity extends AppCompatActivity {
+    public static final String SUPERTEMA_KEY= "supertema";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityDetailBinding binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Bundle extras=getIntent().getExtras();
+        SuperTema superTema=extras.getParcelable(SUPERTEMA_KEY);
+        binding.heroName.setText(superTema.getName());
+        binding.alterEgoText.setText(superTema.getAlertEgo());
+        binding.bioText.setText(superTema.getBio());
+        binding.ratingBar.setRating(superTema.getPower());
+    }
+}
